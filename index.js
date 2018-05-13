bbdc324a92a5b899c57194b6fd20e03b00cb6dcf
FelipeGerolomo <felipegerolomo@gmail.com>
2018-05-13 14:14:48 -0300

    Teste com anexo

--- index.js
+++ index.js
@@ -80,25 +80,65 @@ app.get('/webhook', (req, res) => {
 function handleMessage(sender_psid, received_message) {
     let response;
 
-    // Check if the message contains text
+    // Checks if the message contains text
     if (received_message.text) {
-
-        // Create the payload for a basic text message
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
         response = {
-            "text": `Boa Noite é o caralho!!`
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
         }
     }
 
+    // Send the response message
     callSendAPI(sender_psid, response);
 }
-
-
+// Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
-
-
+// Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+    // Construct the message body
     let request_body = {
         "recipient": {
             "id": sender_psid
@@ -106,16 +146,17 @@ function callSendAPI(sender_psid, response) {
         "message": response
     }
 
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": "EAAFbvdkTpjcBAAdDh6iTviG5BccPuZCyjQnTiRKGDtWP8IIcawZC02IiKzBBaMllkVjmhbsBQuPEDpq3Pmyxypl7zKZACN3i4kVXTsXQIBiLVMi1EiqR33jicJdl8vKjGtmt9hTYz7ZAepElNJZAAz075ZBw93Uol6oEZCG3W3ZCHKQlp6yP7IqF" },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  }); 
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": "EAAFbvdkTpjcBAAdDh6iTviG5BccPuZCyjQnTiRKGDtWP8IIcawZC02IiKzBBaMllkVjmhbsBQuPEDpq3Pmyxypl7zKZACN3i4kVXTsXQIBiLVMi1EiqR33jicJdl8vKjGtmt9hTYz7ZAepElNJZAAz075ZBw93Uol6oEZCG3W3ZCHKQlp6yP7IqF" },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 }
